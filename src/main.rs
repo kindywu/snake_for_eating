d4a3b66727140310cb5ecf3f77a4d374de8efd80
kindywu <kindywu@outlook.com>
2024-06-21 20:51:01 +0800

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
 use bevy::prelude::*;
 
+// 常量，蛇头颜色
+const SNAKE_HEAD_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
+
+// 结构体，蛇头
+#[derive(Component)]
+struct SnakeHead;
 fn main() {
-    App::new().run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, (setup_camera, spawn_snake))
+        .run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+fn spawn_snake(mut commands: Commands) {
+    commands
+        .spawn(SpriteBundle {
+            sprite: Sprite {
+                color: SNAKE_HEAD_COLOR,
+                ..default()
+            },
+            transform: Transform {
+                scale: Vec3::new(10.0, 10.0, 10.0),
+                ..default()
+            },
+            ..default()
+        })
+        .insert(SnakeHead);
 }
